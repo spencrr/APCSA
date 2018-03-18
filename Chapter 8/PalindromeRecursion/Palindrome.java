d75f1d4fe6f8f09cb927d8d10822a426ac6be752
spencrr <23708360+spencrr@users.noreply.github.com>
2018-03-18 13:38:19 -0500

    chapter 8 started

--- Chapter 8/PalindromeRecursion/Palindrome.java
+++ Chapter 8/PalindromeRecursion/Palindrome.java
@@ -2,11 +2,8 @@ public class Palindrome{
 
     public static boolean testPalindrome(String s){
         s = s.toLowerCase();
-        if(s.length() <= 2){
-            if(s.length() <= 1)
-                return true;
-            return firstAndLast(s);
-        }
+        if(s.length() <= 1)
+            return true;
         return firstAndLast(s) && testPalindrome(s.substring(1, s.length() - 1));
     }
 
